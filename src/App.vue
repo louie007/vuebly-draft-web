a96fac6eb2da30c290741876e352f6b1450ca646
Louie Bao <louie.bao@vuebly.com>
2017-07-17 21:43:30 +0800

    Init Project

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="wrapper" @click="update">
+    <text class="title">Bonjour {{target}}!</text>
+  </div>
+</template>
+
+<style>
+  .wrapper { align-items: center; padding-top: 120px; }
+  .title { font-size: 48px; }
+  .logo { width: 360px; height: 82px; }
+</style>
+
+<script>
+  export default {
+    data: {
+      target: 'Vuebly'
+    },
+    methods: {
+      update: function (e) {
+        this.target = 'World'
+        console.log('target:', this.target)
+      }
+    }
+  }
+</script>
---
+++ src/main.js
@@ -0,0 +1,3 @@
+import App from './App.vue'
+App.el = '#root'
+export default new Vue(App);
